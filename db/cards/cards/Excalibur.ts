fba659a38f848920c657bb0b10d7b92e4efde3f7
Givikuna <thommaster10@gmail.com>
2026-03-21 01:11:17 -0400

    Cmpleted Card

--- db/cards/cards/Excalibur.ts
+++ db/cards/cards/Excalibur.ts
@@ -1,10 +1,44 @@
-/*
+import { Board } from "../../../classes/Board";
+import { Player } from "../../../classes/Player";
+import { Card } from "../../../classes/Card";
+import { Action } from "../../../classes/Action";
+
 import { ICard } from "../../../interfaces/ICard";
+
+import { ActionType, CardClass, Faction, HealthClass, Rarity } from "../../../types/enums";
+import { DamageInstance } from "../../../classes/DamageInstance";
 
 export const Excalibur: ICard = {
     name: "Excalibur",
     uid: "CEX001",
 
-    maxHealth:
+    maxHealth: 250,
+    maxShields: 200,
+    overguard: 0,
+
+    cardClass: CardClass.Attacker,
+    faction: Faction.Tenno,
+    healthClass: HealthClass.Tenno,
+    rarity: Rarity.Common,
+    actions: [
+        {
+            action: new Action(
+                "Excalibur",
+                "A01CEX001",
+                "I001",
+                ActionType.OnTurn,
+                "Deals damage",
+                (card: Card, player: Player, board: Board, _actionType: ActionType): void => {
+                    DamageInstance.init(
+                        board[`getPlayer${player.getPlayerNumber() === 1 ? 2 : 1}`]().getCards()[0],
+                        card,
+                        { Slash: 25 },
+                        27,
+                        13,
+                        1.5,
+                    ).apply(player);
+                },
+            ),
+        },
+    ],
 };
-*/
